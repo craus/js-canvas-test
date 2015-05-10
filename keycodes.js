ed61eda5ed8705adfa43d88de7c576462800e4af
Pavel Semushin <craus902@gmail.com>
2015-05-10 14:39:48 +0400

    keycodes instead of magic constants

--- keycodes.js
+++ keycodes.js
@@ -1,4 +1,14 @@
 keycodes = {
   del: 46,
-  pageDown: 34
+  pageDown: 34,
+  left: 37,
+  up: 38, 
+  right: 39,
+  down: 40,
+  character: function(c) { return c.charCodeAt(0) },
+  closeBracket: 221,
+  openBracket: 219,
+  home: 36,
+  esc: 27,
+  space: 32, // this possibly should be character(' ')
 }
